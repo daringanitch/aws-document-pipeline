150a8b9f84b83be69146567b8ef65202665cdf1e
daringanitch <dganitch@gmail.com>
2018-09-19 15:30:15 -0700

    Update docs-repo.tf

--- code-pipeline/docs-repo.tf
+++ code-pipeline/docs-repo.tf
@@ -62,9 +62,11 @@ resource "aws_iam_role_policy" "attach_codepipeline_policy" {
         {
             "Action": [
                 "s3:GetObject",
+                "s3:ListBucket",
                 "s3:GetObjectVersion",
                 "s3:GetBucketVersioning",
-                "s3:PutObject"
+                "s3:PutObject",
+                "s3:DeleteObject"
             ],
             "Resource": "*",
             "Effect": "Allow"
@@ -125,6 +127,8 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       "Action": [
        "s3:PutObject",
        "s3:GetObject",
+       "s3:ListBucket",
+       "s3:DeleteObject",
        "s3:GetObjectVersion",
        "s3:GetBucketVersioning"
       ],
